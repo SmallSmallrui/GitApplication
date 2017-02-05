563965ec4b574518f4a83df2eb183df8b280be5b
hurui <807115007@qq.com>
2017-02-05 21:57:34 +0800

    frist commit second commit

--- app/src/main/java/com/hurui/gitapplication/MainActivity.java
+++ app/src/main/java/com/hurui/gitapplication/MainActivity.java
@@ -1,13 +1,34 @@
 package com.hurui.gitapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyTouchView touchView ;//滑动View
+
+    private TextView xTv ; //显示x轴上面的速度
+    private TextView yTv ; //显示y轴上面的速度
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        touchView = (MyTouchView) findViewById(R.id.myView);
+
+        xTv = (TextView) findViewById(R.id.xTv);
+
+        yTv = (TextView) findViewById(R.id.yTv);
+
+
+        touchView.setCallBack(new MyTouchView.CallBack() {
+            @Override
+            public void shudu(int xVeloct, int yVeloct) {
+                xTv.setText(xVeloct + "px/s");
+                yTv.setText(yVeloct + "px/s");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hurui/gitapplication/MyTouchView.java
@@ -0,0 +1,95 @@
+package com.hurui.gitapplication;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
+import android.widget.Scroller;
+
+/**
+ * Created by lenovo on 2017/2/5.
+ */
+
+public class MyTouchView extends View {
+
+    private CallBack callBack;
+
+    public void setCallBack(CallBack callBack) {
+        this.callBack = callBack;
+    }
+
+    public interface CallBack {
+        public void shudu(int xVeloct, int yVeloct);
+    }
+
+    private VelocityTracker velocityTracker;
+    private int mPointerId;
+
+    public MyTouchView(Context context) {
+        super(context);
+    }
+
+    public MyTouchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTouchView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+
+        if (velocityTracker == null) {
+            velocityTracker = VelocityTracker.obtain();
+        }
+        velocityTracker.addMovement(event);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPointerId = event.getPointerId(0);
+                Log.e("test" , "ACTION_DOWN" ) ;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                velocityTracker.computeCurrentVelocity(1000);
+                int xVelocty = (int) velocityTracker.getXVelocity();//x轴上面的速度
+                int yVelocty = (int) velocityTracker.getYVelocity();//y轴上面的速度
+                if (callBack != null) {
+                    callBack.shudu(xVelocty, yVelocty);
+                }
+                Log.e("test" , "action_move" ) ;
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.e("test" , "action_up") ;
+                smoothScrollTo((int) event.getX(), (int) event.getY());
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                if (velocityTracker != null) {
+                    velocityTracker.recycle();
+                }
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    Scroller scroller = new Scroller(getContext());
+
+    private void smoothScrollTo(int destX , int dextY){
+        int scrollX = getScrollX() ;
+        int delta = destX - scrollX ;
+        scroller.startScroll(scrollX , 0 , delta , 0 , 1000);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        if(scroller.computeScrollOffset()){
+            scrollTo(scroller.getCurrX() , scroller.getCurrY() );
+            postInvalidate();
+        }
+    }
+}
